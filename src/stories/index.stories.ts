89d8f187e22dd7d3f33b77518843fa2a0d1b9409
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-08-22 02:13:30 -0400

    Added second story

--- src/stories/index.stories.ts
+++ src/stories/index.stories.ts
@@ -3,8 +3,21 @@ import { ClockComponent  } from '../clock/clock.component';
 import { TimeService } from '../clock/time.service';
 
 storiesOf('ClockComponent', module)
+
   .add('default', () => ({
     component: ClockComponent,
+    moduleMetadata: {
+      providers: [TimeService]
+    }
+  }))
+
+  .add('red ring', () => ({
+    component: ClockComponent,
+    props: {
+      ringColor: "red",
+      textColor: "red",
+      backgroundColor: "pink"
+    },
     moduleMetadata: {
       providers: [TimeService]
     }
