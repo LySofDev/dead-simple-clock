cf5aa273d893bfcd4cf6dcb54c8fd6f09899658e
Esteban Hernandez <hernandez.dev.services@gmail.com>
2018-08-22 02:31:21 -0400

    Added BrowserModule to Imports

--- src/clock/clock.module.ts
+++ src/clock/clock.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { TimeService } from './time.service';
 import { ClockComponent } from './clock.component';
 
 @NgModule({
-  imports: [],
-  providers: [ TimeService ],
-  declarations: [ ClockComponent ],
-  exports: [ ClockComponent ]
+  imports: [BrowserModule],
+  providers: [TimeService],
+  declarations: [ClockComponent],
+  exports: [ClockComponent]
 })
 export class DeadSimpleClockModule {}
